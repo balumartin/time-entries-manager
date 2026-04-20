2b79423907fa98ecea5bc32d2f1eb80525a2512f
balumartin <martinka.balazs@gmail.com>
2026-04-20 15:40:53 +0200

    update home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,28 +1,9 @@
-import React, { useEffect, useState } from "react";
 import DailySummary from "../components/Summary/DailySummary";
 import WeeklySummary from "../components/Summary/WeeklySummary";
+import { useTimeEntries } from "../context/TimeEntriesContext";
 
 export default function Home() {
-  const [entries, setEntries] = useState([]);
-
-  useEffect(() => {
-    const fetchEntriesFromLocalStorage = () => {
-      const localValue = localStorage.getItem("timeEntries");
-      if (localValue == null) return;
-
-      try {
-        const parsedEntries = JSON.parse(localValue);
-        setEntries(parsedEntries);
-      } catch (err) {
-        console.error(
-          "An error occurred while parsing the localStorage data:",
-          err
-        );
-      }
-    };
-
-    fetchEntriesFromLocalStorage();
-  }, []);
+  const {entries} = useTimeEntries();
 
   return (
     <main className="p-4 flex">
@@ -30,10 +11,10 @@ export default function Home() {
         <p>Daily summary</p>
         <DailySummary entries={entries} selectedDate={Date.now()} />
       </div>
-      <div className="">
+      {/* <div className="">
         <p>Weekly summary</p>
         <WeeklySummary entries={entries} selectedDate={Date.now()}/>
-      </div>
+      </div> */}
     </main>
   );
 }
